f666be5dcd3de089cdd341d2e7aa55aeea3ca6e0
agomez9 <a.greyfox4@gmail.com>
2025-03-06 11:45:48 -0700

    Create gitTesting01.py
    commit to main

---
+++ gitTesting01.py
@@ -0,0 +1,26 @@
+
+
+print("---")
+
+##from os import strerror
+##
+##try: 
+##    for x in open("C:/Users/agrey/Desktop/Python/notes/tzop.txt", "rt"):
+##        print(x)
+##except IOError as e:
+##    print("I/O error occurred: ", strerror(e.errno))
+
+import random
+
+##for i in range(10):
+##    print(random.randrange(0, 10, 3))
+
+for i in range(20):
+    print(random.randrange(10, 100, 3))
+
+##for i in range(10):
+##    print(random.choice((0, 10, 3)))
+
+print("end.")
+
+#C:\Users\agrey\Desktop\Python\notes
